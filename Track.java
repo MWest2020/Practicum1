2eeb287346445403f33b69c552de9dbca0cd394b
Mark West <markwesterweel@conduction.nl>
2022-02-27 10:31:08 +0100

    first files

---
+++ Track.java
@@ -0,0 +1,12 @@
+public class Track {
+    private String title;
+    private int duration;
+
+    public void setTitle(String title){
+        this.title = title;
+    };
+    public void setDuration(int duration){
+        this.duration = duration;
+    }      
+
+}
